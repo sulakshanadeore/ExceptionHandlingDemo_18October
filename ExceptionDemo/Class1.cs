4aad8966f39aedc8faefc9f5d3d0185c57b0cd9d
deoresulakshana <deoresulakshana@gmail.com>
2023-10-18 10:35:06 +0530

    Add project files.

---
+++ ExceptionDemo/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDemo
+{
+    public class MathsCalculations
+    {
+
+        public string SayHello(string name)
+        {
+            string op = null;
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("Didn't receive the name input");
+                //Console.WriteLine("didnt recieve the name-- name is null or empty");
+            }
+            else
+            {
+                op = "Good day, " + name;
+
+            }
+            return op;
+        }
+        public string Greet(string name)
+        {
+            string op = null;
+            try
+            {
+                if (string.IsNullOrEmpty(name))
+                {
+                     throw new ArgumentNullException("Didn't receive the name input");
+                    //Console.WriteLine("didnt recieve the name-- name is null or empty");
+                }
+                else
+                {
+                    op= "Good day, " + name;
+
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("From library= " + ex.Message);
+                Console.WriteLine("didnt receive valid input for name");
+            }
+            return op;
+        }
+        public void Divide(int? i, int? j)
+        {
+            int?  k;
+            try
+            {
+                k = i / j;
+                Console.WriteLine(k);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("You entered the second number as 0 and hence the exception");
+                //Console.WriteLine($"From library ={ex.StackTrace}");
+                //where did the exception occur along with line number
+                //Console.WriteLine($"From library = {ex.Source}");
+                //Name of the program/app where the app occured
+                //Console.WriteLine($"From library= {ex.Message}");
+                //Message of exception
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(  "Some error occured....");
+
+            }
+
+
+        }
+
+    }
+}
---
+++ ExceptionHandlingDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExceptionDemo;
+namespace ExceptionHandlingDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                MathsCalculations cal = new MathsCalculations();
+                Console.WriteLine("Enter no1 ");
+                int? no1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter no1 ");
+                int? no2 = Convert.ToInt32(Console.ReadLine());
+
+                cal.Divide(no1, no2);
+
+                //string name = "";
+                //if (name=="")
+                //{
+                //    throw new ArgumentException("String is empty");
+                //}
+                //else
+                //{
+                //    //  string v=cal.SayHello  (name);
+                //    string v = cal.Greet(name);
+                //    Console.WriteLine(v);
+                //}
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Pls enter valid number");
+                //Console.WriteLine(ex.StackTrace);//where did the exception occur along with line number
+                //Console.WriteLine(ex.Source);//Name of the program/app where the app occured
+                //Console.WriteLine(ex.Message);//Message of exception--
+            }
+            catch (NullReferenceException ex)
+            {
+
+                Console.WriteLine("From program = " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally fired......");
+                //closing connections, releasing resouces is done in the finally,
+                //Always fired whether there is exception or not.
+
+            }
+            Console.Read();
+        }
+    }
+}
